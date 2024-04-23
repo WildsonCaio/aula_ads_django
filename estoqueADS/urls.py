f32ac1e80348ebfd64bbb39120cb67714ae8edbd
WILDSON CAIO FELIPE <wildson.felipe@prof.sc.senac.br>
2024-04-23 20:43:02 -0300

    update

--- estoqueADS/urls.py
+++ estoqueADS/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('adicionar-produto/', views.adicionar_produto, name='adicionar-produto')
 ]
--- estoqueADS/views.py
+++ estoqueADS/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render, HttpResponse
+from .models import Produtos
+from datetime import datetime
 
 def index(request):
-  
-    return HttpResponse(x)
+    produtos = Produtos.objects.all()
+    return render(request, 'pages/index.html', {'produtos':produtos})
+
+def adicionar_produto(request):
+
+    if request.method == "POST":
+        nome = request.POST['nome']
+        preco = request.POST['preco']
+        descricao = request.POST['descricao']
+        quantidade = request.POST['quantidade']
+        codigo = request.POST['codigo']
+        em_estoque = False
+        if quantidade > 0:
+            em_estoque = True 
+        data_criacao = datetime.now()
+
+
+        return render(request, 'pages/adicionar_produto.html')
+
+    else:
+        return render(request, 'pages/adicionar_produto.html')
+    
+
+
+# GET - OBTER DADOS
+# POST - ENVIAR DADOS
+# PUT - ATUALIZAR DADOS
+# PATCH - ATUALIZAÇÃO PARCIAL DE DADOS    
